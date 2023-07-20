b3af54abdb68aa06f7681f9f405fbcc074027f6c
luispablovelasco <89947424+luispablovelasco@users.noreply.github.com>
2023-07-19 18:31:14 -0600

    Modificando formulario

--- resources/views/GestionarUsuarios/index.blade.php
+++ resources/views/GestionarUsuarios/index.blade.php
@@ -22,7 +22,7 @@
             <div class="row justify-content-center">
                 <div class="row mt-5 mb-4"></div>
                 <div class="card border-dark mt-5">
-                    <div class="card-header border-dark mt-3 ">
+                    <div class="card-header border-dark mt-3">
                         <center><h2 class="card-title">Añadir nuevo usuario</h2></center>
                     </div>
                     @if($errors->any())
